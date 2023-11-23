cb641136200601ff02dc9f90be46578c8ff91c54
alissoninacio1 <alissoninacio96@gmail.com>
2023-11-23 15:07:18 -0700

    outro commit

--- App.tsx
+++ App.tsx
@@ -2,18 +2,30 @@ import { Text, View } from "react-native";
 
 export default function App(){
   return (
-    <View>
-      <Text>React Native</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: "#131016",
+      padding: 24
+    }}>
+
+      <Text style={{
+        color: "#FDFCFE",
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 48,
+      }}>
+        Event Name
+      </Text>
+
+      <Text style={{
+        color: "#6B6B6B",
+        fontSize: 16
+      }}>
+        Friday, 11th November, 2023
+      </Text>
+
     </View>
   )
 }
 
 
-//o que eu quero exibir em tela, esta no meu return
-//padrao e: uma funcao que retorna XML
-//nao posso retornar mais de um elemento, 
-//neste caso posso colocar eles dentro de um fragmento <> </>, que serve para embrulhar elementos
-
-//View - similar a uma div
-
-//nome de arquivos com letra maiuscula e com a extensao tsx.
