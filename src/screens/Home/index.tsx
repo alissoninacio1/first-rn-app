fa0326eeca8bca984b7c191e7485a76233ebaa1d
alissoninacio1 <alissoninacio96@gmail.com>
2023-12-28 22:27:43 -0700

    button and basic events added

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View} from "react-native";
+import { Text, View, TextInput, TouchableOpacity} from "react-native";
 
 import { styles } from "./styles";
 
 export function Home(){
+
+  function handleParticipantAdd() {
+    console.log("you clicked")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -13,6 +18,17 @@ export function Home(){
         Friday, 11th November, 2023
       </Text>
 
+      <TextInput 
+      style={styles.input}
+      placeholder="Participant Name"
+      placeholderTextColor="#6b6b6b"
+      />
+
+     <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+      <Text style={styles.buttonText}>
+        +
+      </Text>
+     </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -8,7 +8,7 @@ export const styles = StyleSheet.create({
       padding: 24
     },
     eventName: {
-      color: "#FDFCFE",
+      color: "#fff",
       fontSize: 24,
       fontWeight: "bold",
       marginTop: 48,
@@ -16,6 +16,26 @@ export const styles = StyleSheet.create({
     eventDate: {
       color: "#6B6B6B",
       fontSize: 16
+    },
+    input:{
+      height: 56,
+      backgroundColor:'#1f1e25',
+      borderRadius: 5,
+      color: "#fff",
+      padding: 16,
+      fontSize: 16,
+    },
+    buttonText: {
+      color: "#fff",
+      fontSize: 24,
+    },
+    button: {
+      width: 56,
+      height: 56,
+      borderRadius: 5,
+      backgroundColor: "#31cf67",
+      alignItems: "center",
+      justifyContent: "center",
     }
   });
   
